95813f44b649e5970b63781d5b35014ae4739182
mmetalab <chengwangsdu@outlook.com>
2023-08-14 22:37:15 +0800

    Update 04_CCS_Prediction.py

--- pages/04_CCS_Prediction.py
+++ pages/04_CCS_Prediction.py
@@ -63,7 +63,7 @@ if ccs_button: # Make button a condition.
    for t in features:
       feats.append(t[3])
    feats = np.array(feats)
-   model_file = './models/'+abbr_mode[mode]+'_rf_fingerprint.sav'
+   model_file = './Models/'+abbr_mode[mode]+'_rf_fingerprint.sav'
    model = pickle.load(open(model_file, 'rb'))
    result = model.predict(feats)
    df_f['ccs_fp'] = result
@@ -73,7 +73,7 @@ if ccs_button: # Make button a condition.
    for t in features:
       feats.append(t[2])
    feats = np.array(feats)
-   model_file = './models/'+abbr_mode[mode]+'_rf_descriptor.sav'
+   model_file = './Models/'+abbr_mode[mode]+'_rf_descriptor.sav'
    model = pickle.load(open(model_file, 'rb'))
    result = model.predict(feats)
    df_f['ccs_md'] = result
